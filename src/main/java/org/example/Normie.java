7fc241400a1ec1c45e165daa546a9f6bc2188fd4
ondre <jk.ciechanowski@gmail.com>
2023-05-28 11:53:25 +0200

    modified:   src/main/java/org/example/Normie.java 	modified:   src/main/java/org/example/Simulation.java

--- src/main/java/org/example/Normie.java
+++ src/main/java/org/example/Normie.java
@@ -10,14 +10,6 @@ public class Normie extends Citizen{
         super(money);
     }
 
-    public boolean HaveMated() {
-        return mated;
-    }
-
-    public void Mated(boolean mated) {
-        this.mated = true;
-    }
-
     @Override
     public char getSymbol() {
         return 'N';
